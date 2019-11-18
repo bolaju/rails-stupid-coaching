5a3739876c0677eb979e1f3701ea2e02693503ca
Justin Boediono <justin.boediono@gmail.com>
2019-11-18 14:15:13 +0800

    stupid coach

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'pages#question'
+  get 'question', to: 'pages#question', as: :question
+  get 'answer', to: 'pages#answer', as: :answer
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
